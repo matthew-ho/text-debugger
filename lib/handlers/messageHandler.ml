8f984d2e61416a7205491091a5b90e2959cf4fcc
Matthew Ho <matthew_ho@hotmail.co.uk>
2021-02-16 10:37:09 +0000

    Send StopOnException when exception line contains the word exception

--- lib/handlers/messageHandler.ml
+++ lib/handlers/messageHandler.ml
@@ -3,6 +3,10 @@ let rec run () =
     let end_msg_rec = ({command = "end"} : Protocol.ResponseMessage.t) in
     let end_msg = Json.json_to_string (Protocol.ResponseMessage.to_yojson end_msg_rec) in
     print_endline end_msg
+  else if State.File.has_hit_exception () then
+    let exception_msg_rec = ({command = "stopOnException"} : Protocol.ResponseMessage.t) in
+    let exception_msg = Json.json_to_string (Protocol.ResponseMessage.to_yojson exception_msg_rec) in
+    print_endline exception_msg
   else
     let () = State.File.next_line () in
     run ()
--- lib/state/file.mli
+++ lib/state/file.mli
@@ -3,3 +3,5 @@ val open_file : string -> unit
 val has_ended : unit -> bool
 
 val next_line : unit -> unit
+
+val has_hit_exception : unit -> bool
